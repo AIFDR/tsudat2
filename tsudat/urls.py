0fcf631edca0f06863a94766bcad16640786ba38
Jeffrey Johnson <jjohnson@opengeo.org>
2011-01-31 15:11:05 +0700

    Added a few new urlconfs

--- tsudat/urls.py
+++ tsudat/urls.py
@@ -2,8 +2,11 @@ from django.conf.urls.defaults import *
 
 urlpatterns = patterns('tsudat.views',
     (r'^return_periods/$', 'return_periods'),
+    (r'^return_period/$', 'return_period'),
     (r'^hazard_points/$', 'hazard_points'),
     (r'^source_zones/$', 'source_zones'),
+    (r'^source_zone/$', 'source_zone'),
     (r'^sub_faults/$', 'sub_faults'),
     (r'^events/$', 'events'),
+    (r'^wave_height/$', 'wave_height'),
 )
