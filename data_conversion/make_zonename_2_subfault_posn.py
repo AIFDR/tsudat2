04aa102fb376d544af01db2a2d3a7fd753dc2cf9
Ross Wilson <rzzzwilson@gmail.com>
2011-02-11 15:55:30 +0700

    Made make_zonename_2_subfault_posn.py use 'raw' data.  Same results as ole code.

--- data_conversion/make_zonename_2_subfault_posn.py
+++ data_conversion/make_zonename_2_subfault_posn.py
@@ -14,6 +14,7 @@ This is for loading TsuDATA data into the TsuDAT2 database.
 
 import sys
 import os.path
+import re
 
 
 # default data path - ovridden by sys.argv[1]
@@ -21,42 +22,106 @@ DataPath = '/media/537510fd-c89e-442d-8be0-3163f1bbe59b/Tsu-DAT_Data'
 
 OutputFile = './zonename_2_subfault_posn.csv'
 
-def main():
-    # override path to data directory if sys.argv[1] supplied
-    if len(sys.argv) > 1:
-        global DataPath
-        DataPath = sys.argv[1]
+# generate 're' pattern for 'any number of spaces'
+SpacesPattern = re.compile(' +')
 
-    # generate path to subfaults.txt
-    input_file = os.path.join(DataPath, 'subfaults.txt')
 
-    # get data from file
-    fd = open(input_file, 'r')
+def get_max_subfaultid(datapath):
+    """Get the maximum subfault ID in the data.
+
+    Get value from number of lines in multimux/i_invall minus 4 because:
+        . 3 header lines in i_invall
+        . subfault IDs start at 0
+    """
+
+    fname = os.path.join(datapath, 'multimux', 'i_invall')
+    fd = open(fname, 'r')
     lines = fd.readlines()
     fd.close()
 
-    out_fd = open(OutputFile, 'wb')
+    return len(lines) - 4
 
-    # read data lines, writing to stdout
-    prev_zone = None
-    
+def get_zone_subfault_dict(datapath):
+    """Get a dictionary mapping {subfaultID: <zonename>, ...}.
+
+    datapath  base path of earthquake data
+
+    Returns a dictionary holding all subfaultIDs mapped to a zone name.
+    """
+
+    # read zone_subfault.txt and get dict {0: 'Alaska', ...}
+    # containing the start subfault OD: zone data.
+    start_subfault_2_zone = {}
+    fname = os.path.join(datapath, 'hazmap_files', 'zone_subfault.txt')
+    fd = open(fname, 'r')
+    lines = fd.readlines()
+    fd.close()
+
     for line in lines:
         line = line.strip()
-        if not line or line[0] == '#':
-            continue
+        (zone, id) = line.split(' ')
+        id = int(id)
+        start_subfault_2_zone[id] = zone
 
-        (lon, lat, subfaultid, zonename) = line.split(' ', 3)
-        lon = float(lon)
-        lat = float(lat)
-        subfaultid = int(subfaultid)
+    return start_subfault_2_zone
 
-        if zonename != prev_zone:
-            if prev_zone:
-                out_fd.write('\n')
-            out_fd.write('%s' % zonename)
-            prev_zone = zonename
 
-        out_fd.write(',%.5f:%.5f' % (lon, lat))
+def get_subfaultid_2_posn(datapath):
+    """Get a dictionary containing {<subfaultID>: <posn>, ...}
+
+    datapath  path to base of data doirectory
+    """
+
+    fname = os.path.join(datapath, 'multimux', 'i_invall')
+    fd = open(fname, 'r')
+    lines = fd.readlines()
+    fd.close()
+
+    # skip 3 heder lines
+    lines = lines[3:]
+
+    d = {}
+    for (id, line) in enumerate(lines):
+        line = line.strip()
+        (lon, lat, _) = SpacesPattern.split(line, 2)
+        d[id] = '%s:%s' % (lon, lat)
+
+    return d
+
+    
+def main():
+    # override path to data files if sys.argv[1] supplied
+    if len(sys.argv) > 1:
+        global DataPath
+        DataPath = sys.argv[1]
+    DataPath = os.path.join(DataPath, 'earthquake_data')
+
+    # get the max subfault ID from the data
+    max_subfault = get_max_subfaultid(DataPath)
+
+    # get start subfault -> zone dictionary
+    start_subfault_2_zone = get_zone_subfault_dict(DataPath)
+
+    # get dict of subfaultID: <posn>,
+    # where <posn> is string "<lon>:<lat>"
+    subfaultID_2_posn = get_subfaultid_2_posn(DataPath)
+
+    # open the output file
+    out_fd = open(OutputFile, 'wb')
+
+    # step through all subfault IDs
+    zone_name = None
+    positions = ''
+    for id in xrange(max_subfault + 1):
+        if id in start_subfault_2_zone:
+            # dump any accumulated positions
+            if positions:
+                out_fd.write('%s%s\n' % (zone_name, positions))
+            zone_name = start_subfault_2_zone[id]
+            positions = ''	# happens for id==0
+        positions += ',%s' % subfaultID_2_posn[id]
+        
+    out_fd.write('%s%s\n' % (zone_name, positions))
 
     out_fd.close()
 
