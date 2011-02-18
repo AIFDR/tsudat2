2064c105f23473e92b62cedae69985e31dca2825
Jeff Johnson <jjohnson@obiwan.openplans.org>
2011-02-18 00:15:04 +0000

    Adding hardcoded paths to import GeoNode dependencies

--- manage.py
+++ manage.py
@@ -1,9 +1,14 @@
 #!/usr/bin/env python
+import sys
 from django.core.management import execute_manager
+
+sys.path.append('/var/www/geonode/wsgi/geonode/src/gsconfig.py/src/')
+sys.path.append('/var/www/geonode/wsgi/geonode/src/owslib/')
+sys.path.append('/var/www/geonode/wsgi/geonode/lib/python2.6/site-packages/')
+
 try:
     import settings # Assumed to be in the same directory.
 except ImportError:
-    import sys
     sys.stderr.write("Error: Can't find the file 'settings.py' in the directory containing %r. It appears you've customized things.\nYou'll have to run django-admin.py, passing it your settings module.\n(If the file settings.py does indeed exist, it's causing an ImportError somehow.)\n" % __file__)
     sys.exit(1)
 
--- wsgi/tsudat2.wsgi
+++ wsgi/tsudat2.wsgi
@@ -1,5 +1,9 @@
 import site, os
 
+
+site.addsitedir('/var/www/geonode/wsgi/geonode/src/gsconfig.py/src/')
+site.addsitedir('/var/www/geonode/wsgi/geonode/src/owslib/')
+site.addsitedir('/var/www/geonode/wsgi/geonode/lib/python2.6/site-packages/')
 site.addsitedir('/home/jjohnson/tsudat2/lib/python2.6/site-packages')
 site.addsitedir('/home/jjohnson')
 site.addsitedir('/home/jjohnson/tsudat2')
