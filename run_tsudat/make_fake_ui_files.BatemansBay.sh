96324f30c0b926f388cc456de63ae192e2652412
Ross Wilson <rzzzwilson@gmail.com>
2011-03-11 16:07:09 +0700

    Finalising the 'make the data dir' scripts.

--- run_tsudat/make_fake_ui_files.BatemansBay.sh
+++ run_tsudat/make_fake_ui_files.BatemansBay.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 #
-# A script to recreate the fake_ui_files.VictorHarbour data directory
+# A script to recreate the fake_ui_files.BatemansBay data directory
 # We don't put this directory into git as it's big!
 #
 # A better approach might be to keep a zipped copy of the data on alamba
@@ -11,10 +11,10 @@
 PW=VBi1bu335
 
 # where to create fake_ui data
-TARGET=fake_ui_files.VictorHarbour
+TARGET=fake_ui_files.BatemansBay
 
 # source base on alamba
-SOURCE=/model_area/inundation/data/south_australia/victor_harbor_tsunami_scenario_2010/anuga
+SOURCE=/model_area/inundation/data/new_south_wales/batemans_bay_tsunami_scenario_2009/anuga
 
 # don't run if target dir exists
 if [ -d $TARGET ]; then
@@ -28,18 +28,17 @@ mkdir -p $TARGET
 mkdir -p $TARGET/boundaries
 mkdir -p $TARGET/gauges
 mkdir -p $TARGET/polygons
+mkdir -p $TARGET/topographies
 mkdir -p $TARGET/raw_elevations
 
 # copy required files from alamba
 scp wilsor@alamba:$SOURCE/boundaries/landward_boundary.csv $TARGET/boundaries
 scp wilsor@alamba:$SOURCE/boundaries/urs_order.csv $TARGET/boundaries
-scp wilsor@alamba:$SOURCE/boundaries/58342/victor_harbor.sts $TARGET/boundaries/VictorHarbour.sts
-scp wilsor@alamba:$SOURCE/gauges/gauges_final.csv $TARGET/gauges
+scp wilsor@alamba:$SOURCE/boundaries/58348/batemans_bay.sts $TARGET/boundaries/BatemansBay.sts
+scp wilsor@alamba:$SOURCE/gauges/gauges.csv $TARGET/gauges
 scp wilsor@alamba:$SOURCE/polygons/area_of_interest.csv $TARGET/polygons
 scp wilsor@alamba:$SOURCE/polygons/area_of_significance.csv $TARGET/polygons
 scp wilsor@alamba:$SOURCE/polygons/bounding_polygon.csv $TARGET/polygons
 scp wilsor@alamba:$SOURCE/polygons/shallow_water.csv $TARGET/polygons
-scp wilsor@alamba:$SOURCE/topographies/250m_final.csv $TARGET/raw_elevations
-scp wilsor@alamba:$SOURCE/topographies/aoi.csv $TARGET/raw_elevations
-scp wilsor@alamba:$SOURCE/topographies/shallow_water.csv $TARGET/raw_elevations
+scp wilsor@alamba:$SOURCE/topographies/batemans_bay_combined_elevation.pts $TARGET/topographies/combined_elevation.pts
 
