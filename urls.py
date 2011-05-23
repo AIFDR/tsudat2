cc84d6c184c78a42296566a57b1becf3e9808e8e
root <root@tsudat.(none)>
2011-05-23 15:42:56 +1000

    Merging geonodes urls.py and tsudats urls.py to follow newer structure for projects that extend from GeoNode

--- urls.py
+++ urls.py
@@ -1,25 +1,62 @@
 from django.conf.urls.defaults import *
-from staticfiles.urls import staticfiles_urlpatterns
 from django.conf import settings
+from staticfiles.urls import staticfiles_urlpatterns
+
+# Uncomment the next two lines to enable the admin:
 from django.contrib import admin
+admin.autodiscover()
 
 PROXY_DOMAIN = "localhost:8080/geoserver"
 PROXY_FORMAT = u"http://%s/%s" % (PROXY_DOMAIN, u"%s")
+
+js_info_dict = {
+    'packages': ('geonode.maps',),
+}
+
+urlpatterns = patterns('',
+    # Example:
+    # (r'^geonode/', include('geonode.foo.urls')),
+    (r'^(?:index/?)?$', 'geonode.views.index'),
+    (r'^(?P<page>help)/?$', 'geonode.views.static'),
+    (r'^developer/?$', 'geonode.views.developer'),
+    (r'^lang\.js$', 'geonode.views.lang'),
+    (r'^maps/', include('geonode.maps.urls')),
+    (r'^proxy/', 'geonode.proxy.views.proxy'),
+    (r'^geoserver/','geonode.proxy.views.geoserver'),
+    url(r'^data/$', 'geonode.maps.views.browse_data', name='data'),
+    url(r'^data/acls/?$', 'geonode.maps.views.layer_acls', name='layer_acls'),
+    url(r'^data/search/?$', 'geonode.maps.views.search_page', name='search'),
+    url(r'^data/search/api/?$', 'geonode.maps.views.metadata_search', name='search_api'),
+    url(r'^data/search/detail/?$', 'geonode.maps.views.search_result_detail', name='search_result_detail'),
+    url(r'^data/api/batch_permissions/?$', 'geonode.maps.views.batch_permissions'),
+    url(r'^data/api/batch_delete/?$', 'geonode.maps.views.batch_delete'),
+    url(r'^data/upload$', 'geonode.maps.views.upload_layer', name='data_upload'),
+    (r'^data/download$', 'geonode.maps.views.batch_layer_download'),
+    (r'^data/(?P<layername>[^/]*)$', 'geonode.maps.views.layerController'),
+    (r'^data/(?P<layername>[^/]*)/ajax-permissions$', 'geonode.maps.views.ajax_layer_permissions'),
+    (r'^admin/', include(admin.site.urls)),
+    (r'^i18n/', include('django.conf.urls.i18n')),
+    (r'^jsi18n/$', 'django.views.i18n.javascript_catalog', js_info_dict),
+    (r'^accounts/ajax_login$', 'geonode.views.ajax_login'),
+    (r'^accounts/ajax_lookup$', 'geonode.views.ajax_lookup'),
+    (r'^accounts/login', 'django.contrib.auth.views.login'),
+    (r'^accounts/logout', 'django.contrib.auth.views.logout'),
+    (r'^avatar/', include('avatar.urls')),
+    (r'^accounts/', include('registration.urls')),
+    (r'^profiles/', include('profiles.urls')),
+    (r'^admin/', include(admin.site.urls)),
+    (r'^proxy/', 'proxy.views.proxy'),
+    (r'^geoserver/','proxy.views.geoserver'),
+    (r'^tsudat/', include('tsudat.urls')),
+    #url(r'^$', 'tsudat.views.index', name='tsudat-index'),
+    )
+
+# Extra static file endpoint for development use
+if settings.SERVE_MEDIA:
+    urlpatterns += staticfiles_urlpatterns()
+
 
 def build_pattern(name, action="show"):
         return url("^%s/(?P<object_id>\d+)/%s$" % (name, action),
             "%s_%s" % (action, name),
             name="%s_%s" % (action, name))
-
-admin.autodiscover()
-
-urlpatterns = patterns('',
-    url(r'^$', 'tsudat.views.index', name='tsudat-index'),
-    (r'^admin/', include(admin.site.urls)),
-    (r'^proxy/', 'proxy.views.proxy'),
-    (r'^geoserver/','proxy.views.geoserver'),
-    (r'^tsudat/', include('tsudat.urls')),
-)
-
-if settings.SERVE_MEDIA:
-    urlpatterns += staticfiles_urlpatterns()
