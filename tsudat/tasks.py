0aa3c7b90b17469fcb8036d1ceaa3a85882c0f16
Jeffrey Johnson <jjohnson@opengeo.org>
2011-05-26 08:59:28 +1000

    In progress work on tsudat/tasks.py

--- tsudat/tasks.py
+++ tsudat/tasks.py
@@ -15,12 +15,14 @@ from tsudat.models import *
 from celery.decorators import task
 from geonode.maps.models import *
 
-#import gdal
-#from gdalconst import *
-#import osr
+from osgeo import gdal
+from gdalconst import *
+import osr
 
 from util.LatLongUTMconversion import LLtoUTM 
-#from run_tsudat import run_tsudat
+from run_tsudat import run_tsudat
+
+import re
 
 try:
     from notification import models as notification
@@ -29,21 +31,43 @@ except ImportError:
 
 logger = logging.getLogger("tsudat2.tsudat.tasks")
 
+# Move somewhere more accessible
+_slugify_strip_re = re.compile(r'[^\w\s-]')
+_slugify_hyphenate_re = re.compile(r'[-\s]+')
+def _slugify(value):
+    """
+    Normalizes string, converts to lowercase, removes non-alpha characters,
+    and converts spaces to hyphens.
+    
+    From Django's "django/template/defaultfilters.py".
+    """
+    import unicodedata
+    if not isinstance(value, unicode):
+        value = unicode(value)
+    value = unicodedata.normalize('NFKD', value).encode('ascii', 'ignore')
+    value = unicode(_slugify_strip_re.sub('', value).strip().lower())
+    return _slugify_hyphenate_re.sub('-', value)
+
 @task
 def run_tsudat_simulation(user, scenario_id):
+    """
+    Generate a working directory for running ANUGA
+    Create/Copy all of the files to that directory that are necessary
+    for running the simulation. Generate a json run file and call run_tsudat.
+    Notes here: https://github.com/AIFDR/tsudat2/wiki/Create-anuga-run-script
+    """
+
     # Get the scenario object from the Database
     scenario = Scenario.objects.get(id=scenario_id)
     
     # the base of the TsuDAT user directory structures from settings.py 
     TsuDATBase = settings.TSUDAT_BASE_DIR
     TsuDATMux = settings.TSUDAT_MUX_DIR
-    DataFilesDir = '%s/fake_ui_files.%s' % ('/home/software/tsudat2/run_tsudat', scenario.project.name)
-    print DataFilesDir
 
     # create the user working directory
-    #(run_dir, raw_elevations, boundaries, meshes,
-            # polygons, gauges) = run_tsudat.make_tsudat_dir(TsuDATBase, user.username, scenario.project.name,
-                    #                                        scenario.name, scenario.model_setup, scenario.event.tsudat_id)
+    (work_dir, raw_elevations, boundaries, meshes,
+             polygons, gauges) = run_tsudat.make_tsudat_dir(TsuDATBase, user.username, _slugify(scenario.project.name),
+                                                            _slugify(scenario.name), scenario.model_setup, scenario.event.tsudat_id)
 
     project_geom = scenario.project.geom
     project_extent = scenario.project.geom.extent
@@ -68,7 +92,8 @@ def run_tsudat_simulation(user, scenario_id):
     for coord in project_geom.coords[0]:
         bounding_polygon_file.write('%f,%f\n' % (coord[0], coord[1]))
     bounding_polygon_file.close()
-  
+ 
+    # Internal Polygons 
     internal_polygons = InternalPolygon.objects.filter(project=scenario.project).order_by('value')
     count = 0
     InteriorRegions = []
@@ -78,7 +103,7 @@ def run_tsudat_simulation(user, scenario_id):
         geom.transform(srid)
         for coord in geom.coords[0]:
             ipfile.write('%f,%f\n' % (coord[0], coord[1]))
-        InteriorRegions.append([ipfile.name, ip.value])
+        InteriorRegions.append([ip.type, ipfile.name, ip.value])
         ipfile.close()
         geom = ipfile = None
         count += 1
@@ -87,21 +112,22 @@ def run_tsudat_simulation(user, scenario_id):
     RawElevationFiles = []
 
     wcs_url = settings.GEOSERVER_BASE_URL + 'wcs'
-
-    '''
     wcs = WebCoverageService(wcs_url, version='1.0.0')
     pds = ProjectDataSet.objects.filter(project=scenario.project).order_by('ranking')
-    output_format = "AAIGrid"
-    driver = gdal.GetDriverByName(output_format)
     srs = osr.SpatialReference()
     srs.ImportFromEPSG(srid)
     dst_wkt = srs.ExportToPrettyWkt()
     eResampleAlg = None
     create_options = None
+    
+    output_format = "AAIGrid"
+    driver = gdal.GetDriverByName(output_format)
+    
     for ds in pds:
-        layer = Layer.objects.using('geonode').get(uuid=ds.dataset.geonode_layer_uuid)
-        metadata = wcs.contents[layer.name]
-	print metadata.grid
+        layer = Layer.objects.get(uuid=ds.dataset.geonode_layer_uuid)
+        logger.info(wcs.contents)
+        metadata = wcs.contents[layer.typename]
+        print metadata.grid
         resx = metadata.grid.offsetvectors[0][0]
         resy = abs(float(metadata.grid.offsetvectors[1][1]))
         formats = metadata.supportedFormats
@@ -128,15 +154,33 @@ def run_tsudat_simulation(user, scenario_id):
         cmd = "/usr/bin/gdalwarp -t_srs EPSG:%d %s %s.tmp" % (srid, tif_file_path, tif_file_path)
         os.system(cmd)
         # Convert to AAIGrid
-        cmd = "/usr/bin/gdal_translate -of %s %s.tmp %s" % (output_format, tif_file_path, asc_file_path)
+        cmd = "/usr/bin/gdal_translate -a_nodata -9999 -of %s %s.tmp %s" % (output_format, tif_file_path, asc_file_path)
         os.system(cmd)
         # Remove Intermediate files
-        #os.remove(tif_file_path)
-        #os.remove(tif_file_path + ".tmp")
-        
+        os.remove(tif_file_path)
+        os.remove(tif_file_path + ".tmp")
+      
+        # Rename the .prj file to .prj.wkt
+        shutil.move(asc_file_path.replace('.asc', '.prj'), asc_file_path.replace('.asc', '.prj.wkt'))
+         
+        # Generate a prj.adf style prj file
+        # NOTE: Not sure if this will work in all cases?
+        prj_file_name = '%s.prj' % ds.ranking
+        prj_file = open(os.path.join(raw_elevations, prj_file_name), 'w')
+        prj_file.write('Projection    UTM\n')
+        prj_file.write('Zone          %d\n' % utm_zone)
+        prj_file.write('Datum         WGS1984\n')
+        prj_file.write('Zunits        NO\n')
+        prj_file.write('Units         METERS\n')
+        prj_file.write('Spheroid      WGS_1984\n')
+        prj_file.write('Xshift        500000\n')
+        prj_file.write('Yshift        10000000\n')
+        prj_file.write('Parameters\n')
+        prj_file.close()        
+
         RawElevationFiles.append(asc_file_path)
-    '''
-    '''
+         
+        '''
         src_ds = gdal.Open( str(tif_file_path), GA_ReadOnly )
         dst_ds_tmp = driver.CreateCopy( str(asc_file_name + '.tmp'), src_ds, 0)
         dst_ds = driver.Create( str(asc_file_path), dst_ds_tmp.RasterXSize, dst_ds_tmp.RasterYSize)
@@ -144,15 +188,26 @@ def run_tsudat_simulation(user, scenario_id):
         dst_ds = None
         dst_ds_tmp = None
         src_ds = None
-    '''
+        '''
 
-    # Boundaries TODO
-    LandwardBoundary = 'landward_boundary.csv'
-    shutil.copy2(os.path.join(DataFilesDir, 'boundaries', LandwardBoundary), boundaries)
-    UrsOrder = 'urs_order.csv'
-    shutil.copy2(os.path.join(DataFilesDir, 'boundaries', UrsOrder), boundaries)
-    STSFile = '%s.sts' % scenario.project.name
-    shutil.copy2(os.path.join(DataFilesDir, 'boundaries', STSFile), boundaries)
+    # Landward Boundary
+    landward_boundary_file = open(os.path.join(boundaries, 'landward_boundary.csv'), 'w')
+    for coord in project_geom.coords[0]:
+        pnt_wkt = 'SRID=%s;POINT(%f %f)' % (srid, coord[0], coord[1])
+        logger.debug(pnt_wkt)
+        land = Land.objects.filter(the_geom__intersects=pnt_wkt)
+        if(land.count() > 0):
+            landward_boundary_file.write('%f,%f\n' % (coord[0], coord[1]))
+    landward_boundary_file.close()
+
+    # Interior Hazard Points File
+    interior_hazard_points_file = open(os.path.join(boundaries, 'interior_hazard_points.csv'), 'w')
+    hps = HazardPoint.objects.filter(geom__intersects=project_geom).order_by('tsudat_id')
+    for hp in hps:
+        the_geom = hp.geom
+        the_geom.transform(srid)
+        interior_hazard_points_file.write('%f,%f\n' % (the_geom.coords[0], the_geom.coords[1]))
+    interior_hazard_points_file.close()
     
     # Gauges
     gauge_file = open(os.path.join(gauges, 'gauges.csv'), 'w')
@@ -163,15 +218,8 @@ def run_tsudat_simulation(user, scenario_id):
         gauge_geom.transform(srid)
         gauge_file.write('%f,%f,%s,%f\n' % (gauge_geom.coords[0], gauge_geom.coords[1], gauge.name, 0.0))
     gauge_file.close()
-    
-    # Topographies TODO
-    MeshFile = 'meshes.msh'
-    Elevation = 'combined_elevation.pts'
-
-    # pre-generated combined elevation data file
-    topo = os.path.join(os.path.dirname(gauges), 'topographies')
-    shutil.copy2(os.path.join(DataFilesDir, 'topographies', Elevation), topo)
-
+   
+    # Layers 
     scenario_layers = scenario.output_layers.all()
     layers = []
     for layer in scenario_layers:
@@ -179,46 +227,41 @@ def run_tsudat_simulation(user, scenario_id):
 
     # build the scenario json data file
     date_time = strftime("%Y%m%d%H%M%S", gmtime()) 
-    json_file = os.path.join(run_dir, '%s.%s.%s.json' % (scenario.project.name, scenario.name, date_time))
+    json_file = os.path.join(work_dir, '%s.%s.json' % (_slugify(scenario.name), date_time))
                
-    json_dict = {'working_directory': TsuDATBase,
-                 'mux_directory': TsuDATMux,
-                 'user': user.username,
-                 'project': scenario.project.name,
-                 'scenario_name': scenario.name,
-                 'setup': scenario.model_setup,
-                 'event': scenario.event.tsudat_id,
-                 'tide': scenario.initial_tidal_stage,
-                 'start_time': scenario.start_time,
-                 'end_time': scenario.end_time,
-                 'smoothing': scenario.smoothing_param,
-                 'bounding_polygon': bounding_polygon_file.name,
-                 'elevation_data': RawElevationFiles,
-                 'mesh_friction': scenario.default_friction_value,
-                 'raster_resolution': scenario.raster_resolution,
-                 'layers': layers, 
-                 'area': ['All'], #TODO?
-                 'get_results_max': True,
-                 'get_timeseries': True,
-                 'gauges': gauge_file.name,
-                 'meshfile': MeshFile,
-                 'interior_regions_data': InteriorRegions,
-                 'bounding_polygon_maxarea': scenario.project.max_area,
-                 'urs_order': UrsOrder,
-                 'landward_boundary': LandwardBoundary,
-                 'ascii_grid_filenames': [],
-                 'zone': utm_zone,
-                 'xminAOI': project_geom.extent[0],
-                 'yminAOI': project_geom.extent[1],
-                 'xmaxAOI': project_geom.extent[2],
-                 'ymaxAOI': project_geom.extent[3],
-                 'force_run': False, # if True, *forces* a simulation
-                 'debug': True}	# if True, forces DEBUG logging
+    json_dict = {
+                    'user': user.username,
+                    'project': scenario.project.name,
+                    'scenario_name': scenario.name,
+                    'setup': scenario.model_setup,
+                    'event_number': scenario.event.tsudat_id,
+                    'working_directory': TsuDATBase,
+                    'mux_directory': TsuDATMux,
+                    'initial_tide': scenario.initial_tidal_stage,
+                    'start_time': scenario.start_time,
+                    'end_time': scenario.end_time,
+                    'smoothing': scenario.smoothing_param,
+                    'bounding_polygon_file': bounding_polygon_file.name,
+                    'elevation_data_list': RawElevationFiles,
+                    'mesh_friction': scenario.default_friction_value,
+                    'raster_resolution': scenario.raster_resolution,
+                    'export_area': "AOI" if scenario.use_aoi == True else "ALL",
+                    'gauge_file': gauge_file.name,
+                    'bounding_polygon_maxarea': scenario.project.max_area,
+                    'interior_regions_list': InteriorRegions,
+                    'interior_hazard_points_file': interior_hazard_points_file.name, 
+                    'landward_boundary': landward_boundary_file.name,
+                    'zone': utm_zone,
+                    'layers_file': layers, 
+                    'get_results_max': True,
+                    'get_timeseries': True 
+                }
+
     with open(json_file, 'w') as fd:
         json.dump(json_dict, fd, indent=2, separators=(',', ':'))
 
     # now run the simulation
-    #run_tsudat.run_tsudat(json_file)
+    run_tsudat.run_tsudat(json_file)
 
     # Setup the new layers in the GeoNode
 
