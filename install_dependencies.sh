806f19ebcd66d97663cebe76e5e4402c1052b904
Jeffrey Johnson <jjohnson@opengeo.org>
2011-06-09 08:42:33 +1000

    Updated install_dependencies.sh for new notification implementation

--- install_dependencies.sh
+++ install_dependencies.sh
@@ -1,9 +1,12 @@
 source ../geonode/wsgi/geonode/bin/activate
 export PATH=$PATH:/usr/pgsql-9.0/bin/
 pip install django-celery
-pip install django-notification
+pip install -e git+git://github.com/jtauber/django-notification.git#egg=django_notification
 pip install django-kombu
 pip install geojson
 pip install vectorformats
 pip install wadofstuff-django-serializers 
 pip install psycopg2
+pip install -e git+git://github.com/ericflo/django-pagination.git#egg=django_pagination
+pip install pytz
+pip install django-timezones
