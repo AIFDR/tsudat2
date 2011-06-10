64a0f917a2e7b85fd33d313fa71dc531c30d8302
Jeffrey Johnson <jjohnson@opengeo.org>
2011-06-10 16:55:39 +1000

    New files from testing.

---
+++ run_tsudat/ncios_bootstrap.py
@@ -0,0 +1,330 @@
+#!/usr/bin/env python
+
+"""
+The TsuDAT2 bootstrap.
+
+The minimum code required to run a TsuDAT simulation.
+
+Expects the user/project/scenario/setup values in user data.
+Gets the S3 package, unzips it and runs run_tsudat.py in the
+scripts directory.
+"""
+
+
+import os
+import sys
+import gc
+import shutil
+import zipfile
+import glob
+import json
+import time
+import tempfile
+import traceback
+
+import boto
+import messaging_amqp as amqp
+
+import tsudat_log as log
+
+
+# the file to log to
+LogFile = 'tsudat.log'
+
+# Instance information URL
+InstanceInfoURL = 'http://169.254.169.254'
+
+# URL to get user-data from
+UserDataURL = '%s/2007-01-19/user-data' % InstanceInfoURL
+
+# URL to get instance ID
+InstanceURL = '%s/latest/meta-data/instance-id' % InstanceInfoURL
+
+# sub-directory holding run_tsudat.py and other scripts/data
+ScriptsDirectory = 'scripts'
+
+# name of the JSON file in the S3 input data
+JSONFile = 'data.json'
+
+# number of minutes to keep idle machine alive -  the idea is to keep instance
+# alive for debugging.  for nice message order, IdleTime should not be an
+# integer multiple of ReminderTime.
+IdleTime = 30 - 1	# 0.5 hour (almost)
+ReminderTime = 5
+
+# message status strings
+StatusStart = 'START'
+StatusStop = 'STOP'
+StatusAbort = 'ABORT'
+StatusIdle = 'IDLE'
+StatusLog = 'LOG'
+StatusError = 'ERROR'
+
+
+def abort(msg):
+    """Abort a run with an error message.
+
+    We assume logging has been set up.
+    """
+
+    log.critical('ABORT: %s' % msg)
+
+    send_message(status=StatusAbort, message=msg)
+
+    # then stop the AMI
+    shutdown()
+
+def error(msg):
+    """Send an ERROR message.
+
+    We assume logging has NOT been set up.
+    Stop the process right here.  Can't use shutdown().
+    """
+
+    log.critical('ERROR: %s' % msg)
+
+    send_message_lite(status=StatusERROR, message=msg)
+
+    # then stop the AMI
+    os.system('/usr/bin/euca-terminate-instances %s' % Instance)
+
+
+def wait_a_while():
+    """Wait for a specified number of seconds (global).
+
+    Send reminder message every few (global) minutes.
+    """
+
+    elapsed_time = 0
+    while elapsed_time < IdleTime:
+        send_message(status=StatusIdle)
+        time.sleep(ReminderTime*60)
+        elapsed_time += ReminderTime
+
+
+def shutdown():
+    """Shutdown this AMI."""
+
+    log.debug('Debug is %s, instance is %sterminating immediately'
+              % (str(Debug), 'not ' if Debug else ''))
+
+    if Debug:
+        wait_a_while()
+        send_message(status=StatusStop)
+
+    os.system('/usr/bin/euca-terminate-instances %s' % Instance)
+
+
+def send_message(**kwargs):
+    """Send a message to server.
+
+    kwargs   a dict of keyword arguments
+
+    Send a JSON representation of the kwargs dict.
+    Add the User, Project, Scenario, Setup global values.
+    """
+
+    # add the global values
+    kwargs['user'] = User
+    kwargs['project'] = Project
+    kwargs['scenario'] = Scenario
+    kwargs['setup'] = Setup
+    kwargs['instance'] = Instance
+
+    # add time as float and string (UTC, ISO 8601 format)
+    kwargs['time'] = time.time()
+    kwargs['timestamp'] = time.strftime('%Y-%m-%d %H:%M:%SZ', time.gmtime())
+
+    # get JSON string
+    msg = json.dumps(kwargs)
+    log.debug('message JSON: %s' % msg)
+
+    amqp.post_server_message(msg)
+    
+
+def send_message_lite(**kwargs):
+    """Send a message.  Can't assume any environment.
+
+    kwargs   a dict of keyword arguments
+
+    Send a JSON representation of the kwargs dict ONLY.
+    Can't assume logging is set up
+    """
+
+    # add time as float and string (UTC, ISO 8601 format)
+    kwargs['timestamp'] = time.strftime('%Y-%m-%d %H:%M:%SZ', time.gmtime())
+
+    # get JSON string
+    msg = json.dumps(kwargs)
+
+    amqp.post_server_message(msg)
+    
+
+def run_tsudat_log(msg=None):
+    """Callback routine for ANUGA periodic logging.
+
+    msg  the message to send to SQS with status of 'LOG'
+    """
+
+    if msg is None:
+        msg = 'Progress message'
+
+    send_message(message=msg, status=StatusLog)
+
+
+def bootstrap():
+    """Bootstrap the TsuDAT run into existence.
+
+    The following globals are used (all are strings):
+    User      user name
+    Project   the TsuDAT project
+    Scenario  the scenario
+    Setup     the run setup ('trial', etc)
+    BaseDir   base of the tsudat working directory
+    """
+
+    log.info('bootstrap start, user_data globals:')
+    log.info('   User=%s' % User)
+    log.info('   UserDir=%s' % UserDir)
+    log.info('   Project=%s' % Project)
+    log.info('   Scenario=%s' % Scenario)
+    log.info('   Setup=%s' % Setup)
+    log.info('   BaseDir=%s' % BaseDir)
+    log.info('   Debug=%s' % Debug)
+    log.info('   Instance=%s' % Instance)
+
+    send_message(status=StatusStart)
+
+    # jigger the PYTHONPATH so we can import 'run_tsudat' from the common filesystem
+    new_pythonpath = os.path.join(UserDir, Project, Scenario, Setup,
+                                  ScriptsDirectory)
+    sys.path.append(new_pythonpath)
+    log.debug('Added additional import path=%s' % new_pythonpath)
+
+    # get the code for the rest of the simulation
+    import run_tsudat
+
+    # get path to the JSON file in scripts dir, pass to run_tsudat()
+    json_path = os.path.join(new_pythonpath, JSONFile)
+    log.info('Running run_tsudat.run_tsudat(%s)' % json_path)
+    gen_files = run_tsudat.run_tsudat(json_path)
+
+    # add local log files to the 'log' entry
+    gen_files['log'] = glob.glob('*.log')
+
+    # before we possibly delete the gen_files['sww'], get output path
+    save_zip_base = os.path.dirname(gen_files['sww'][0])[1:]
+    log.debug('save_zip_base=%s' % save_zip_base)
+
+    # if user data shows 'getsww' as False, remove 'sww' key from dictionary
+    if not UserData.get('GETSWW', True):
+        msg = 'Userdata says not to save SWW files, deleting...'
+        log.info(msg)
+        send_message(status=StatusLog, msg=msg)
+        del gen_files['sww']
+
+    # optionally dump returned file data
+    if Debug:
+        import pprint
+        pp = pprint.PrettyPrinter(indent=4)
+        gen_str = pprint.pformat(gen_files)
+        log.debug('Returned files:\n%s' % gen_str)
+
+    # convert gen_files to JSON and add to stopping message
+    gen_files_json = json.dumps(gen_files)
+    send_message(status=StatusStop, payload=gen_files_json)
+
+    # stop this AMI
+    log.info('run_tsudat() finished, shutting down')
+    shutdown()
+
+
+if __name__ == '__main__':
+    import re
+
+    global UserData, User, Project, Scenario, Setup, BaseDir, Debug, Instance
+    global UserDir
+
+    def excepthook(type, value, tb):
+        """Exception hook routine."""
+
+        msg = '\n' + '='*80 + '\n'
+        msg += 'Uncaught exception:\n'
+        msg += ''.join(traceback.format_exception(type, value, tb))
+        msg += '='*80 + '\n'
+        print(msg)		# we can't assume logging is set up
+        abort(msg)
+
+    # plug our handler into the python system
+    sys.excepthook = excepthook
+        
+    # get instance ID
+    with os.popen('wget -O - -q %s' % InstanceURL) as fd:
+        Instance = fd.readline()
+
+    # get user-data into a string & convert JSON to python dictionary
+    with os.popen('wget -O - -q %s' % UserDataURL) as fd:
+        json_userdata = fd.readlines()
+
+    json_userdata = '\n'.join(json_userdata)
+    UserData = json.loads(json_userdata)
+
+    # uppercase all dictionary keys
+    for (key, value) in UserData.items():
+        del UserData[key]
+        UserData[key.upper()] = value
+
+    # check userdata payload
+    error_flag = False
+    error_msg = []
+
+    if not UserData.get('USER', ''):
+        error_flag = True
+        error_msg.append("USER field doesn't exist or is empty")
+    if not UserData.get('USERDIR', ''):
+        error_flag = True
+        error_msg.append("USERDIR field doesn't exist or is empty")
+    if not UserData.get('PROJECT', ''):
+        error_flag = True
+        error_msg.append("PROJECT field doesn't exist or is empty")
+    if not UserData.get('SCENARIO', ''):
+        error_flag = True
+        error_msg.append("SCENARIO field doesn't exist or is empty")
+    if not UserData.get('SETUP', ''):
+        error_flag = True
+        error_msg.append("SETUP field doesn't exist or is empty")
+    if not UserData.get('BASEDIR', ''):
+        error_flag = True
+        error_msg.append("BASEDIR field doesn't exist or is empty")
+
+    if error_flag:
+        msg = '\n'.join(error_msg)
+        print(msg)
+        error(msg)
+
+    # set userdata defaults, etc
+    if not UserData.get('DEBUG', ''):
+        UserData['DEBUG'] = 'production'
+
+    # set globals: common use variables, debug and logging, etc
+    User = UserData['USER']
+    UserDir = UserData['USERDIR']
+    Project = UserData['PROJECT']
+    Scenario = UserData['SCENARIO']
+    Setup = UserData['SETUP']
+    BaseDir = UserData['BASEDIR']
+
+    if UserData['DEBUG'].lower() == 'debug':
+        Debug = True
+        level = log.DEBUG
+    else:
+        Debug = False
+        level = log.INFO
+
+    log = log.Log(LogFile, level=level)
+
+    log.debug('UserData=%s' % str(UserData))
+
+    # do it!
+    bootstrap()
+
---
+++ run_tsudat/start_ami_test.py
@@ -0,0 +1,60 @@
+#!/usr/bin/env python
+
+"""
+Start an AMI.
+
+usage: start_ami_test <ami>
+"""
+
+import os
+import time
+import tempfile
+import boto
+from boto.ec2.regioninfo import RegionInfo
+
+
+# keypair used to start instance
+DefaultKeypair = 'testkey'
+
+# default instance type
+DefaultType = 'c1.large'
+
+
+def start_ami(ami, key_name=DefaultKeypair, instance_type=DefaultType,
+              user_data=None):
+    """Start the configured AMI, wait until it's running.
+
+    ami            the ami ID ('ami-????????')
+    key_name       the keypair name
+    instance_type  type of instance to run
+    user_data      the user data string to pass to instance
+    """
+
+    if user_data is None:
+        user_data = ''
+
+    # create file with user data
+    (fd, tfile) = tempfile.mkstemp(prefix='start_ami_')
+    os.write(fd, user_data)
+    os.close(fd)
+
+    cmd = 'euca-run-instances -f %s -k %s -t %s %s' % (tfile, key_name, instance_type, ami)
+    print('Doing: %s' % cmd) 
+    os.system(cmd)
+
+    time.sleep(1)
+#    os.remove(tfile)
+
+
+if __name__ == '__main__':
+    import sys
+
+    if len(sys.argv) != 2:
+        print __doc__
+        sys.exit(10)
+
+    ami = sys.argv[1]
+
+    user_data="""{"Project":"project","UserDir":"/data/tsudat_runs/user_20110610T023627","Debug":"debug","User":"user","Scenario":"VictorHarbour","ScriptPath":"/data/tsudat_runs/user_20110610T023627/project/VictorHarbour/trial/scripts","Setup":"trial","BaseDir":"/data/tsudat_runs/user_20110610T023627/project/VictorHarbour/trial","JSONData":"/data/tsudat_runs/user_20110610T023627/project/VictorHarbour/trial/scripts/data.json","getsww":true}"""
+
+    start_ami(ami, user_data=user_data)
