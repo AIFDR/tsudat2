c70f284413a503f17abce509846a521b101dac66
Duncan Gray <duncan.gray@ga.gov.au>
2013-03-04 15:31:30 +1100

    adding get-multimux file

--- backend/build_urs_boundary.py
+++ backend/build_urs_boundary.py
@@ -4,7 +4,11 @@ Build the urs boundary.
 
 import sys
 import os
-#import anuga
+import anuga
+
+# name of the fault name file (in multimux directory)
+FaultNameFilename = 'fault_list.txt'
+
 
 
 def create_urs_order(landward_boundary_path, interior_hazard_points_path,
@@ -137,3 +141,78 @@ def get_deformation(mux_event_file, deformation_folder, ouput_stem):
 
     return txt_file
 
+def get_multimux(event, multimux_dir, output_file):
+    """Does exactly what David Burbidge's 'get_multimux' program does.
+
+    event         event ID
+    multimux_dir  path to the multimux files
+    output_file   path to file to write
+    """
+
+    # get data
+    filename = os.path.join(multimux_dir, FaultNameFilename)
+    try:
+        fd = open(filename, "r")
+        fault_names = [ fn.strip() for fn in fd.readlines() ]
+        fd.close()
+    except IOError, msg:
+        raise RuntimeError(1, "Error reading file: %s" % msg)
+
+    # open the output file
+    try:
+        outfd = open(output_file, "w")
+    except IOError, msg:
+        raise Exception('Error opening output file: %s' % msg)
+
+    # handle each fault
+    nquake = 0
+    for fn in fault_names:
+        # create the filename for the multimux data file
+        mmx_filename = 'i_multimux-%s' % fn
+        mmx_filename = os.path.join(multimux_dir, mmx_filename)
+
+        # Read all data in file, checking as we go
+        try:
+            infd = open(mmx_filename, "r")
+        except IOError, msg:
+            raise Exception('Error opening file: %s' % msg)
+
+        # check fault name in file is as expected
+        mux_faultname = infd.readline().strip()
+        if mux_faultname != fn:
+            raise Exception("Error in file %s: fault name in file isn't %s"
+                            % (mmx_filename, fn))
+
+        # read data
+        while True:
+            # get number of subfaults, EOF means finished
+            try:
+                nsubfault = infd.readline()
+            except IOError:
+                raise Exception("Error reading file %s: EOF reading event"
+                                % mmx_filename)
+
+            if not nsubfault:
+                break
+            nsubfault = int(nsubfault)
+
+            nquake += 1
+            if nquake == event:
+                outfd.write(' %d\n' % nsubfault)
+                for i in range(nsubfault):
+                    line = infd.readline()
+                    (subfaultname, slip, prob, mag, _) = \
+                                   SpacesPattern.split(line, maxsplit=4)
+                    subfaultname = subfaultname.strip()
+                    slip = float(slip)
+                    outfd.write(" %s %g\n" % (subfaultname, slip))
+            else:
+                for i in range(nsubfault):
+                    try:
+                        infd.readline()
+                    except IOError:
+                        raise Exception("Something wrong at bottom of file %s"
+                                        % mux_faultname)
+
+        infd.close()
+    outfd.close()
